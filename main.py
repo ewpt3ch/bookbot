f3252a245a82112e3c77c4f72706a89b083db426
Eric Phillips <eric@ewpt3ch.com>
2023-12-20 22:52:35 -0700

    report prints

--- main.py
+++ main.py
@@ -16,13 +16,19 @@ def get_num_letters(text):
             letter_freq[c] = 1
     return letter_freq
 
+
 if __name__ == "__main__":
     print("Reading Frankenstein")
     book = 'books/frankenstein.txt'
     text = read_book(book)
     num_words = get_num_words(text)
     letters = get_num_letters(text)
-    print(letters)
-    print(num_words)
-
-
+    print(f"Report of {book}")
+    print(f"There are {num_words} in the document")
+    letter_list = []
+    for c in letters:
+        if c.isalpha():
+            letter_list.append((c, letters[c]))
+    letter_list.sort()
+    for l in letter_list:
+        print(f"The letter {l[0]} was used {l[1]} times in {book}.")
